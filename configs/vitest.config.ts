0844afe211938a3892c83afa06c314020455d4d5
Ryan Irani <ryan.irani@gmail.com>
2025-05-19 15:19:43 +0200

    chore: change test env from jest to vitest

---
+++ configs/vitest.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './vitest.setup.ts',
+    include: ['src/**/*.{test,spec}.{ts,tsx}'],
+
+    coverage: {
+      provider: 'istanbul',
+      reporter: ['text', 'lcov', 'json-summary'],
+      all: true,
+      clean: true,
+    },
+
+    restoreMocks: true,
+    testTimeout: 10000,
+  },
+});
