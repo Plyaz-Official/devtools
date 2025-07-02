761c618229aeab28fdd452e60991de1b38820111
usmangq12 <usmangq12@gmail.com>
2025-07-02 18:44:36 +0500

    feat: Migrate shared configration

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+/// <reference types="vitest" />
+
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+
+  build: {
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        preserveModules: true,
+        preserveModulesRoot: 'src',
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+});
