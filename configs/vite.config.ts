afcf82403c2c06ad944035743545124b32d9c577
usmangq12 <usmangq12@gmail.com>
2025-08-04 18:28:04 +0500

    chore: Added vite configuration

---
+++ configs/vite.config.ts
@@ -0,0 +1,38 @@
+/// <reference types="vitest" />
+
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig, type PluginOption } from 'vite';
+import { visualizer } from 'rollup-plugin-visualizer';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+    visualizer({
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+      filename: 'performance/reports/bundle-stats.html',
+      exclude: {
+        bundle: 'storybook-static',
+      },
+      template: 'treemap',
+      sourcemap: true,
+    }) as PluginOption,
+  ],
+
+  build: {
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        preserveModules: true,
+        preserveModulesRoot: 'src',
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+});
