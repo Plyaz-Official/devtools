28957c08d5ac262632dd1196092a4315b5615497
Muhammad Usman <usmangq12@gmail.com>
2025-12-03 17:27:24 +0500

    [WIP] feat: Add pipeline for Lighthouse (#22)
    * feat: Add pipeline for Lighthouse
    
    * fix: Fixed build issue
    
    * feat: Worked on custom domain
    
    * fix: Added condition
    
    * fix: Updated lightouse
    
    * fix: Updated lightouse.yml file
    
    * chore: Added engines in `package.json`
    
    * chore: Update pnpm version

--- configs/lighthouserc.cjs
+++ configs/lighthouserc.cjs
@@ -5,7 +5,6 @@ module.exports = {
     collect: {
       url: [process.env.LHCI_URL || 'http://localhost:3000'],
       startServerCommand: process.env.LHCI_START || undefined,
-      numberOfRuns: 2,
       settings: {
         onlyCategories: ['performance', 'accessibility', 'best-practices', 'seo'],
         chromeFlags: '--no-sandbox',
@@ -13,10 +12,10 @@ module.exports = {
     },
     assert: {
       assertions: {
-        'categories:performance': ['warn', { minScore: 0.9 }],
-        'categories:accessibility': ['error', { minScore: 0.95 }],
-        'categories:seo': ['error', { minScore: 0.9 }],
-        'categories:best-practices': ['error', { minScore: 0.9 }],
+        "categories:performance": ["error", { minScore: 0.9 }],
+        "categories:accessibility": ["error", { minScore: 0.95 }],
+        "categories:seo": ["error", { minScore: 0.9 }],
+        "categories:best-practices": ["error", { minScore: 0.9 }],
       },
     },
     upload: {
