395194fd02b98835bb8d665b21d55c73ae295ff0
usmangq12 <usmangq12@gmail.com>
2026-01-06 20:43:37 +0500

    chore: Update index.mjs for TS standard rules.

--- eslint/config/index.mjs
+++ eslint/config/index.mjs
@@ -319,6 +319,8 @@ export function createTypeScriptConfig({ tsconfigDir = process.cwd() } = {}) {
       '@typescript-eslint/no-floating-promises': 'error',
       '@typescript-eslint/prefer-nullish-coalescing': 'error',
       '@typescript-eslint/prefer-optional-chain': 'error',
+      'no-redeclare': 'off',
+      '@typescript-eslint/no-redeclare': 'error',
       '@typescript-eslint/consistent-type-imports': [
         'error',
         {
